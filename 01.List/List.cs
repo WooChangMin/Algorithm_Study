391fa58df6ee44a76e8e1213c7c7cde3d081ef8e
woochangmin <rebuilding33@gmail.com>
2023-04-18 18:02:09 +0900

    docs : list,arraylist,array 차이 추가

---
+++ 01.List/List.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    enum Type { Int, Double, String, Float, Char}
+    internal class List
+    {
+        public List(Type type, int Count)
+        {
+            switch (type)
+            {
+                case Type.Int:
+                    int[] list = new int[Count];
+                    break;
+                case Type.Double:
+                    double[] list = new double[Count];
+                    break;
+                case Type.Float:
+                    int[] list = new int[Count];
+                    break;
+                case Type.String:
+                    int[] list = new int[Count];
+                    break;
+                case Type.Char:
+                    char[] list = new char[Count];
+                    break;
+            }
+        }
+
+
+    }
+}
