d42a59a65f433e6df3110c6990e4a37138c33938
woochangmin <rebuilding33@gmail.com>
2023-04-18 17:49:10 +0900

    feat : list 기본 설명

--- 01.List/Program.cs
+++ 01.List/Program.cs
@@ -1,4 +1,5 @@
-﻿namespace _01.List
+﻿using System.Collections.Generic;
+namespace _01.List
 {
     internal class Program
     {
@@ -57,7 +58,36 @@
 
         static void Main(string[] args)
         {
-            
+            List<string> list = new List<string>();
+
+            //list.Count
+            //list.Capacity
+
+            list.Add("1번 데이터");
+            list.Add("2번 데이터");
+            list.Add("3번 데이터");
+            list.Add("4번 데이터");
+            list.Add("5번 데이터");
+
+            string value;
+            value = list[0];
+            value = list[1];
+            value = list[2];
+            value = list[3];
+            value = list[4];
+
+            list[0] = "5번 데이터";
+            list[1] = "4번 데이터";
+            list[2] = "3번 데이터";
+            list[3] = "2번 데이터";
+            list[4] = "1번 데이터";
+
+            list.Remove("3번 데이터");
+            list.Remove("2번 데이터");
+
+            string? findValue = list.Find(x => x.Contains('4'));
+            int findIndex = list.FindIndex(x => x.Contains('1'));
+
         }
     }
 }
