ed731bad59ad409becbe565f8ae448d2e869a52e
woochangmin <rebuilding33@gmail.com>
2023-04-20 16:58:09 +0900

    feat : list에 임시반복기추가

--- 03.Iterator/List.cs
+++ 03.Iterator/List.cs
@@ -1,12 +1,162 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _03.Iterator
+namespace Iterator
 {
-    internal class List
+    internal class List<T> : IEnumerable<T>
     {
+        private const int DefaultCapacity = 10;
+
+        private T[] items;
+        private int size;
+
+        public List()
+        {
+            this.items = new T[DefaultCapacity];
+            this.size = 0;
+        }
+
+        public int Capacity { get { return items.Length; } }
+        public int Count { get { return size; } }
+        public T this[int index]                           // 인덱서를 활용해서 리스트의 특정 인덱스에 접근가능.
+        {
+            get
+            {
+                if (index < 0 || index >= size)
+                    throw new IndexOutOfRangeException();
+
+                return items[index];
+            }
+            set
+            {
+                if (index < 0 || index >= size)
+                    throw new IndexOutOfRangeException();
+
+                items[index] = value;
+            }
+        }
+
+        public void Add(T item)                            // 특정 리스트에 item을 추가하는 방법 -> 배열보다 큰경우 예외 추가필요
+        {
+            if (size < items.Length)
+            {
+                items[size++] = item;
+            }
+            else
+            {
+                Grow();
+                items[size++] = item;
+
+            }
+        }
+
+
+        public bool Remove(T item)                         // RemoveAt함수를 사용한 Remove 함수 구현
+        {
+            int index = IndexOf(item);
+            if (index >= 0)
+            {
+                //TODO : 지우기작업
+                RemoveAt(index);
+                return true;
+            }
+            else
+            {
+                // 못찾은 경우
+                return false;
+            }
+        }
+
+        public void RemoveAt(int index)                    // Array.Copy를 사용하여 특정 인덱스의 값을 지우고 그 인덱스뒤의 인덱스들의 값을 한칸씩 땡김.
+        {
+            if (index < 0 || index >= size)
+                throw new IndexOutOfRangeException();
+            size--;
+            Array.Copy(items, index + 1, items, index, size - index);
+        }
+
+        public int IndexOf(T item)                          //특정 인덱스의 값을 찾는 방법 int값으로 반환되며 +1 일경우 있음, -1 경우 없음을 나타냄
+        {
+            return Array.IndexOf(items, item, 0, size);
+        }
+
+        public T? Find(Predicate<T> match)
+        {
+            if (match == null)
+                throw new ArgumentNullException("match");
+
+            for (int i = 0; i < size; i++)
+            {
+                if (match(items[i]))
+                    return items[i];
+            }
+
+            return default(T);
+        }
+
+        public int FindIndex(Predicate<T> match)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (match(items[i]))
+                    return i;
+            }
+
+            return -1;
+        }
+        private void Grow()
+        {
+            int newCapacity = items.Length * 2;
+            T[] newitems = new T[newCapacity];
+            Array.Copy(items, 0, newitems, 0, size);
+            items = newitems;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new Enumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new Enumerator();
+        }
+
+        public struct Enumerator : IEnumerator<T>
+        {
+            private List<T> list;
+            private int index;
+
+            public T Current { get { return list[index]; } }
+
+            object IEnumerator.Current => throw new NotImplementedException();
+
+            public void Dispose()
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool MoveNext()
+            {
+                if (index < list.Count - 1)
+                {
+                    index++;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            public void Reset()
+            {
+                index = 0;
+            }
+        }
     }
 }
