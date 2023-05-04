dcdeffea0f20d465c6cdd775c0eb85dd631090f6
우창민 <rebuilding33@gmail.com>
2023-05-04 21:37:48 +0900

    feat : TextRPG 실제구현

---
+++ Project_TextRPG_CM/Demon/Demon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_TextRPG_CM
+{
+    public class Demon
+    {
+        enum Rank {slave, knight, Viscount, Duke, King, Emperor, NPC}
+        
+        public int hp;
+        public int mp;
+        public int gold;
+        public int AttackPoint;
+        public 
+    }
+}
---
+++ Project_TextRPG_CM/Game.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_TextRPG_CM
+{
+    public class Game
+    {
+
+        private string name;
+        private bool running = true;
+
+        public void GameRender()
+        {
+
+        }
+
+        public void GameInput()
+        {
+
+        }
+
+        public void GameUpdate()
+        {
+
+
+        }
+        public void GameOver()
+        {
+            Console.Clear();
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("GameOver");
+            Thread.Sleep(2000);
+            running = false;
+        }
+
+        public void GameClear()
+        {
+            Console.Clear();
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("GameClear");
+            Thread.Sleep(2000);
+            running = false;
+        }
+    }
+}
